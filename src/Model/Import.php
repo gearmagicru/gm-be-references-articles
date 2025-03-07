29a49dd956ec7229194e9de5e980509e86279d72
Anton Tivonenko <93121899+gearmagicru-ti@users.noreply.github.com>
2025-03-07 13:10:43 +0300

    Обновлён Gm\Backend\References\Articles\Model\Import

--- src/Model/Import.php
+++ src/Model/Import.php
@@ -30,68 +30,80 @@ class Import extends \Gm\Import\Import
     {
         return [
             // идентификатор типа 
-            'id' => ['field' => 'id', 'type' => 'int'],
+            'id' => [
+                'field' => 'id', 
+                'type' => 'int'
+            ],
             // название
-            'name' => ['field' => 'name'],
+            'name' => [
+                'field' => 'name', 
+                'length' => 255
+            ],
             // описание
-            'description' => ['field' => 'description'],
+            'description' => [
+                'field' => 'description', 
+                'length' => 255
+            ],
             // URL-путь значка
-            'icon' => ['field' => 'icon'],
+            'icon' => [
+                'field' => 'icon', 
+                'length' => 255
+            ],
             // доступность
-            'enabled' => ['field' => 'enabled', 'type' => 'int'],
+            'enabled' => [
+                'field' => 'enabled', 
+                'type'  => 'int'
+            ],
             // просмотр записей всех материалов
-            'all' => ['field' => 'all', 'type' => 'int'],
+            'all' => [
+                'field' => 'all', 
+                'type'  => 'int'
+            ],
             // имена полей
-            'fields' => ['field' => 'fields'],
+            'fields' => [
+                'field' => 'fields', 
+                'trim' => true
+            ],
             // столбцы
-            'columns' => ['field' => 'columns'],
+            'columns' => [
+                'field' => 'columns', 
+                'trim' => true
+            ],
             // компоненты
-            'components' => ['field' => 'components'],
+            'components' => [
+                'field' => 'components', 
+                'trim'  => true
+            ],
             // элементы дерева
-            'elements' => ['field' => 'elements'],
+            'elements' => [
+                'field' => 'elements', 
+                'trim'  => true
+            ],
             // элементы вкладки "Атрибуты"
-            'tab_attributes' => ['field' => 'tab_attributes'],
+            'tab_attributes' => [
+                'field' => 'tab_attributes', 
+                'trim'  => true
+            ],
             // элементы вкладки "Анонс"
-            'tab_announce' => ['field' => 'tab_announce'],
+            'tab_announce' => [
+                'field' => 'tab_announce', 
+                'trim'  => true
+            ],
             // элементы вкладки "Текст"
-            'tab_text' => ['field' => 'tab_text'],
+            'tab_text' => [
+                'field' => 'tab_text', 
+                'trim'  => true
+            ],
             // элементы вкладки "SEO"
-            'tab_seo' => ['field' => 'tab_seo'],
+            'tab_seo' => [
+                'field' => 'tab_seo', 
+                'trim'  => true
+            ],
             // элементы вкладки "Дополнительно"
-            'tab_additionally' => ['field' => 'tab_additionally']
+            'tab_additionally' => [
+                'field' => 'tab_additionally', 
+                'trim'  => true
+            ]
         ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function afterImportAttributes(array $columns): array
-    {
-        // текст материала
-        if (!empty($columns['text'])) {
-            $columns['text'] = trim($columns['text']);
-        }
-        // текст материала без форматирования
-        if (!empty($columns['text'])) {
-            $columns['text_plain'] = strip_tags($columns['text']);
-        }
-        // анонс материала
-        if (!empty($columns['announce'])) {
-            $columns['announce'] = trim($columns['announce']);
-        }
-        // анонс материала без форматирования
-        if (!empty($columns['announce'])) {
-            $columns['announce_plain'] = strip_tags($columns['announce']);
-        }
-        // дата и время публикации материала
-        if (empty($columns['publish_date']))
-            $columns['publish_date'] = gmdate('Y-m-d H:i:s'); // UTC
-        else
-            $columns['publish_date'] = gmdate('Y-m-d', strtotime($columns['publish_date']));
-        // хэш ярлыка (слага)
-        if (!empty($columns['slug'])) {
-            $columns['slug_hash'] = md5($columns['slug']);
-        }
-        return $columns;
     }
 }
