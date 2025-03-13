0df61678ee5ef1bc85a42e4bedf6908c94b3840b
Anton Tivonenko <93121899+gearmagicru-ti@users.noreply.github.com>
2025-03-13 15:41:45 +0300

    Обновлен Gm\Backend\References\Articles\Model\Import
    Удалён метод afterImportAttributes

--- src/Model/Import.php
+++ src/Model/Import.php
@@ -106,16 +106,4 @@ class Import extends \Gm\Import\Import
             ]
         ];
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function afterImportAttributes(array $columns): array
-    {
-        // URL-путь значка
-        if (!empty($columns['icon'])) {
-            $columns['icon'] = MODULE_BASE_URL . $columns['icon'];
-        }
-        return $columns;
-    }
 }
